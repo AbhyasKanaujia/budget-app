cf010d3d6ce0bcd70334d2762efe42bed0f0f644
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-10-06 00:35:53 +0530

    Add async/await syntax

--- backend/index.js
+++ backend/index.js
@@ -9,99 +9,88 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      if (error.name === "ValidationError") {
-        const errors = Object.keys(error.errors).map((field) => ({
-          field: field,
-          message: error.errors[field].message,
-        }));
-        res.status(400).send({ errors: errors });
-      } else if (
-        error.name === "MongoServerError" &&
-        error.message.startsWith("E11000") &&
-        "email" in error.keyPattern
-      ) {
-        res.status(400).send({
-          errors: [
-            {
-              field: "email",
-              message: "An account with this email ID already exists",
-            },
-          ],
-        });
-      } else {
-        res.status(500).send({ error: "Internal server error" });
-      }
-    });
-});
-
-app.get("/users", (req, res) => {
-  User.find()
-    .then((users) => res.send(users))
-    .catch((error) => res.status(500).send({ error: "Internal server error" }));
-});
-
-app.get("/users/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ error: "User not found" });
-      }
-      res.send(user);
-    })
-    .catch((error) => {
-      console.log(error);
-      if (error.name === "CastError") {
-        res.status(400).send({ error: "Invalid User ID" });
-      } else res.status(500).send({ error: "Internal server error" });
-    });
-});
-
-app.post("/transactions", (req, res) => {
-  const transaction = new Transaction(req.body);
-  transaction
-    .save()
-    .then(() => {
-      res.status(201).send(transaction);
-    })
-    .catch((error) => {
-      if (error.name === "ValidationError") {
-        const errors = Object.keys(error.errors).map((field) => {
-          return {
-            field,
-            message: error.errors[field].message,
-          };
-        });
-        res.status(400).send({ errors });
-      } else res.status(500).send({ error: "Internal server error" });
-    });
-});
-
-app.get("/transactions", (req, res) => {
-  Transaction.find()
-    .then((transactions) => res.send(transactions))
-    .catch((error) => res.status(500).send({ error: "Internal server error" }));
-});
-
-app.get("/transactions/:id", (req, res) => {
-  Transaction.findById(req.params.id)
-    .then((transaction) => {
-      if (!transaction)
-        return res.status(404).send({ error: "Transaction not found" });
-      res.send(transaction);
-    })
-    .catch((error) => {
-      if (error.name === "CastError")
-        return res.status(400).send({ error: "Invalid transaction ID" });
+app.post("/users", async (req, res) => {
+  try {
+    if (await User.findOne({ email: req.body.email })) {
+      return res
+        .status(400)
+        .send({ error: "An account with given email ID already exists" });
+    }
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errors = Object.keys(error.errors).map((field) => ({
+        field: field,
+        message: error.errors[field].message,
+      }));
+      res.status(400).send({ errors: errors });
+    } else {
       res.status(500).send({ error: "Internal server error" });
-    });
+    }
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    res.send(await User.find());
+  } catch (error) {
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    res.send(user);
+  } catch (error) {
+    if (error.name === "CastError")
+      return res.status(404).send({ error: "User not found" });
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.post("/transactions", async (req, res) => {
+  try {
+    const transaction = new Transaction(req.body);
+    await transaction.save();
+    res.status(201).send(transaction);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const errors = Object.keys(error.errors).map((field) => {
+        return {
+          field,
+          message: error.errors[field].message,
+        };
+      });
+      res.status(400).send({ errors });
+    } else res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.get("/transactions", async (req, res) => {
+  try {
+    res.send(await Transaction.find());
+  } catch (error) {
+    res.status(500).send({ error: "Internal server error" });
+  }
+});
+
+app.get("/transactions/:id", async (req, res) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction)
+      return res.status(404).send({ error: "Transaction not found" });
+    res.send(transaction);
+  } catch (error) {
+    if (error.name === "CastError")
+      return res.status(404).send({ error: "Transaction not found" });
+    res.status(500).send({ error: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
