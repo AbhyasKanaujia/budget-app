900a8154adcec4006c51c1116e6405a73ba1b2f8
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-10-11 10:42:56 +0530

    Add user login route

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -26,6 +26,21 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.post("/users/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).send({ error: "Email and password is required" });
+
+  try {
+    const user = await User.findByCredentials(email, password);
+
+    res.send(user);
+  } catch (error) {
+    res.status(401).send({ error: "Unable to login" });
+  }
+});
+
 router.get("/users", async (req, res) => {
   try {
     res.send(await User.find());
@@ -63,7 +78,7 @@ router.patch("/users/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) return res.status(404).send({ error: "User not found" });
-    
+
     updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
 
