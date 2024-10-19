47bacaccc784e1bf0ed407c1d8a4549d3c452939
Abhyas Kumar Kanaujia <k.abhyas@gmail.com>
2024-10-19 21:12:02 +0530

    Add timestamps

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,37 +1,44 @@
 const mongoose = require("mongoose");
 
-const Transaction = mongoose.model("Transaction", {
-  description: {
-    type: String,
-    trim: true,
-  },
-  amount: {
-    type: Number,
-    required: [true, "Amount is required"],
-  },
-  category: {
-    type: String,
-    default: "Other",
-    trim: true,
-  },
-  transactionType: {
-    type: String,
-    enum: {
-      values: ["income", "expense"],
-      message:
-        "{VALUE} is not a valid transactionType, please select income or expense",
+const transactionSchema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+      trim: true,
     },
-    default: "expense",
+    amount: {
+      type: Number,
+      required: [true, "Amount is required"],
+    },
+    category: {
+      type: String,
+      default: "Other",
+      trim: true,
+    },
+    transactionType: {
+      type: String,
+      enum: {
+        values: ["income", "expense"],
+        message:
+          "{VALUE} is not a valid transactionType, please select income or expense",
+      },
+      default: "expense",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    owner: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  owner: {
-    type: mongoose.Schema.ObjectId,
-    required: true,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
--- backend/models/user.js
+++ backend/models/user.js
@@ -4,37 +4,42 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Transaction = require("./transaction");
 
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    trim: true,
-    lowercase: true,
-    unique: true,
-    validate: {
-      validator: (value) => validator.isEmail(value),
-      message: (props) => `${props.value} is not a valid email address`,
-    },
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    trim: true,
-    minlength: [8, "Password must be at least 8 characters long"],
-    validate: {
-      validator: (value) => !value.toLowerCase().includes("password"),
-      message: "Password cannot contain the word password",
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: (props) => `${props.value} is not a valid email address`,
       },
     },
-  ],
-});
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+      minlength: [8, "Password must be at least 8 characters long"],
+      validate: {
+        validator: (value) => !value.toLowerCase().includes("password"),
+        message: "Password cannot contain the word password",
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.virtual("transactions", {
   ref: "Transaction",
